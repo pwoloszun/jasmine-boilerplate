c7695ba22e8da8aa824ab16a32ab42265878a3ef
pwoloszun <piotr.woloszun@gmail.com>
2013-12-03 02:49:37 +0100

    v 1.1

--- js/app/models/range.js
+++ js/app/models/range.js
@@ -3,8 +3,7 @@ var RocketMind = {};
 RocketMind.Range = function(min, max) {
   this._min = min;
   this._max = max;
-}
+};
 
 RocketMind.Range.prototype.include = function(value) {
-  return value > this._min && value < this._max;
 };
