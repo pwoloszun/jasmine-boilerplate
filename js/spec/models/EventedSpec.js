d7fb58d9fc04f54118902f5740080148f69fc7d3
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-01-20 13:23:19 +0100

    Evented specs

--- js/spec/models/EventedSpec.js
+++ js/spec/models/EventedSpec.js
@@ -1,12 +1,46 @@
-//it("should call listener function with given arguments", function() {
-//  var listener = sinon.spy();
-//  var args = ['qq', 123];
-//  evented.on("click", listener);
-//  evented.trigger("click", args);
-//
-//  listener.called.should.be.true;
-//  listener.calledOn(evented).should.be.true;
-//  listener.calledWith(args[0], args[1]).should.be.true;
-//});
+xdescribe("Evented", function() {
+  var object, listener, eventName;
 
-// TODO: specs for Evented
+  beforeEach(function() {
+    object = {};
+    mixin(object, Evented);
+    listener = sinon.spy();
+    eventName = "click";
+    object.on(eventName, listener);
+  });
+
+  it("should call litener function", function() {
+    object.trigger(eventName);
+    listener.called.should.be.true;
+  });
+
+  it("should call listener in evented object context", function() {
+    object.trigger(eventName);
+    listener.calledOn(object).should.be.true;
+  });
+
+  it("should call listener function with given arguments", function() {
+    var args = ['qq', 123];
+    object.trigger(eventName, args);
+    listener.calledWith(args[0], args[1]).should.be.true;
+  });
+
+  it("should call every listener function", function() {
+    var listener2 = sinon.spy();
+    obj.on(eventName, listener2);
+    obj.trigger("click");
+
+    listener.called.should.be.true;
+    listener2.called.should.be.true;
+  });
+
+  it("should call only assigned listener function", function() {
+    var obj2 = {};
+    mixin(obj2, Evented);
+
+    object.trigger(eventName);
+
+    listener.calledOnce.should.be.true;
+    listener.calledOn(obj2).should.be.false;
+  });
+});
