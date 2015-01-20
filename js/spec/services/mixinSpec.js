b9e42919ea7b412fd8d13d2fc151a918d82b403b
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-01-20 12:03:49 +0100

    mixin specs

--- js/spec/services/mixinSpec.js
+++ js/spec/services/mixinSpec.js
@@ -1,1 +1,42 @@
-// TODO: specs for mixin
+xdescribe("mixin", function() {
+  var Encryptable;
+
+  beforeEach(function() {
+    Encryptable = {
+      encrypt: function(propertyName) {
+      },
+      getEncrypted: function(propertyName) {
+      }
+    };
+  });
+
+  describe("extend class prototype", function() {
+    var User, user;
+
+    beforeEach(function() {
+      User = function() {
+      };
+      mixin(User.prototype, Encryptable);
+    });
+
+    it("should extend User prototype", function() {
+      user = new User();
+      for (var propertyName in Encryptable)
+        user[propertyName].should.equal(Encryptable[propertyName]);
+    });
+  });
+
+  describe("extend instance", function() {
+    var bob;
+
+    beforeEach(function() {
+      bob = {name: "Bob"};
+      mixin(bob, Encryptable);
+    });
+
+    it("should extend object instance", function() {
+      for (var propertyName in Encryptable)
+        bob[propertyName].should.equal(Encryptable[propertyName]);
+    });
+  });
+});
