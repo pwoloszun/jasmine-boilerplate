590f08ceaf03b2b5ee2e920340d8b9e75f697c59
Piotr Woloszun <piotr.woloszun@gmail.com>
2015-01-20 08:06:08 +0100

    mixin, Evented, Stateful

--- js/app/services/mixin.js
+++ js/app/services/mixin.js
@@ -1,11 +1,1 @@
-var mixin = (function() {
-  var mixin = function(destination, source) {
-    for (var key in source) {
-      if (source.hasOwnProperty(key))
-        destination[key] = source[key];
-    }
-    return destination;
-  };
-
-  return mixin;
-})();
+// TODO: mixin(destination, source)
---
+++ js/spec/models/EventedSpec.js
@@ -0,0 +1,12 @@
+//it("should call listener function with given arguments", function() {
+//  var listener = sinon.spy();
+//  var args = ['qq', 123];
+//  evented.on("click", listener);
+//  evented.trigger("click", args);
+//
+//  listener.called.should.be.true;
+//  listener.calledOn(evented).should.be.true;
+//  listener.calledWith(args[0], args[1]).should.be.true;
+//});
+
+// TODO: specs for Evented
